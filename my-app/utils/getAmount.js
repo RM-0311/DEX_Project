3f1cef63d7e279b6b4e896d8f514c2aced8e3bdf
unknown <md.ridgeway.work@gmail.com>
2022-04-01 18:56:14 -0500

    getAmount

---
+++ my-app/utils/getAmount.js
@@ -0,0 +1,32 @@
+import { Contract } from "ethers";
+import {
+  EXCHANGE_CONTRACT_ABI,
+  EXCHANGE_CONTRACT_ADDRESS,
+  TOKEN_CONTRACT_ABI,
+  TOKEN_CONTRACT_ADDRESS,
+} from "../constants";
+
+/**
+ * getEtherBalance: Retrieves the ether balance of the user of the contract
+ */
+export const getEtherBalance = async (
+  provider,
+  address,
+  contract = false
+) => {
+  try {
+    // If the caller has set the 'contract' boolean to true, retrieve the balance of 
+    // ether in the 'exchange contract', if it is set to false, retrieve the balance
+    // of the user's address
+    if (contract) {
+      const balance = await provider.getBalance(EXCHANGE_CONTRACT_ADDRESS);
+      return balance;
+    } else {
+      const balance = await provider.getBalance(address);
+      return balance;
+    }
+  } catch (err) {
+    console.error(err);
+    return 0
+  }
+}
