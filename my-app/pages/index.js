2c53ab70d664ad95a056f3ef2530a09fd9eec518
unknown <md.ridgeway.work@gmail.com>
2022-04-24 14:51:06 -0500

    Add input

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -208,5 +208,126 @@ export default function Home() {
     }
   };
 
+/** 
+ * Returns a Provider or Signer object representing the RPC with or without signing capabilities of metamask attached
+ * 
+ * A 'Provider' is needed to interact with the blockchain - read transactions
+ * 
+ * A 'Signer' is a special type of Provider used in the case a 'write' transaction needs to be made to the blockchain, which involves the connected account
+ * needing to make a digital signature to authorize the transaction being sent. Metamask exposes a Signer API to allow your website to
+ * request signatures from the user using Signer functions
+ * 
+ * @param {*} needSigner - True if you need the signer, default false otherwise
+ */
 
+  const getProviderOrSigner = async (needSigner = false) => {
+    // Connect to Metamask
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+
+    // If user is not connected to the Rinkeby network, let them know and throw an error
+    const { chainId } = await web3Provider.getNetwork();
+    if (chainId !== 4) {
+      window.alert("Change the network to Rinkeby");
+      throw new Error("Change network to Rinkeby");
+    }
+
+    if (needSigner) {
+      const signer = web3Provider.getSigner();
+      return signer;
+    }
+    return web3Provider;
+  };
+
+  // useEffects to react to changes
+  // Whenever the value of 'walletConnected' changes - this effect will be called
+  useEffect(() => {
+    // if wallet is not connected, create a new instance of Web3Modal and connect the MetaMask wallet
+    if (!walletConnected) {
+      web3ModalRef.current = new Web3Modal({
+        network: "rinkeby",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+      connectWallet();
+      getAmounts();
+    }
+  }, [walletConnected]);
+
+  /*
+  renderButton: Render a button based on the state of the dapp
+  */
+ const renderButton = () => {
+   // If wallet is not connected, return a button which allows them to connect their wallet
+   if (!walletConnected) {
+     return (
+       <button onClick={connectWallet} className={styles.button}>
+         Connect your wallet
+       </button>
+     );
+   }
+
+   // If we are currently waiting for something, return a loading button
+   if (loading) {
+     return <button className={styles.button}>Loading...</button>;
+   }
+
+   if (liquidityTab) {
+     return (
+       <div>
+         <div className={styles.description}>
+           You have:
+           <br />
+           {/*Convert the BigNumber to string using the formatEther function from ether.js */}
+           {utils.formatEther(cdBalance)} Crypto Dev Tokens
+           <br />
+           {utils.formatEther(ethBalance)} Ether
+           <br />
+           {utils.formatEther(lpBalance)} Crypto Dev LP tokens
+         </div>
+         <div>
+           {utils.parseEther(reservedCD.toString()).eq(zero) ? (
+             <div>
+               <input
+                  type="number"
+                  placeholder="Amount of Ether"
+                  onChange={(e) => setAddEther(e.target.value || "0")}
+                  className={styles.input}
+                />
+                <input 
+                  type="number"
+                  placeholder="Amount of CryptoDev tokens"
+                  onChange={(e) => 
+                    setAddCDTokens(
+                      BigNumber.from(utils.parseEther(e.target.value || "0"))
+                    )
+                  }
+                  className={styles.input}
+                />
+                <button className={styles.button1} onClick={_addLiquidity}>
+                  Add
+                </button>
+              </div>
+           ) : (
+             <div>
+               <input
+                  type="number"
+                  placeholder="Amount of Ether"
+                  onChange={async (e) => {
+                    setAddEther(e.target.value || "0");
+                    // calculate the number of CD tokens that 
+                    // can be added given 'e.target.value' amount of Eth
+                    const _addCDTokens = await calculateCD(
+                      e.target.value || "0",
+                      etherBalanceContract,
+                      reservedCD
+                    );
+                    setAddCDTokens(_addCDTokens);
+                  }}
+                  className={styles.input}    
+              />
+       </div>
+     )
+   }
+ }
 }
